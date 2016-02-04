4361202d4206ed4960841e524db47084950c80fc
Wassim Beltaief <beltaief.wassin@gmail.com>
2016-02-04 18:22:38 +0100

    add method saveMovies that emits movies one by one after save to the Datamanager

--- app/src/main/java/com/wassim/androidmvpbase/data/local/database/DatabaseHelper.java
+++ app/src/main/java/com/wassim/androidmvpbase/data/local/database/DatabaseHelper.java
@@ -3,21 +3,30 @@ package com.wassim.androidmvpbase.data.local.database;
 import com.activeandroid.query.Select;
 import com.wassim.androidmvpbase.data.local.preferences.PreferencesHelper;
 import com.wassim.androidmvpbase.data.model.Movie;
-import com.wassim.androidmvpbase.util.EventPosterHelper;
+import com.wassim.androidmvpbase.util.RxEventBusHelper;
+
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import rx.Observable;
+import rx.Observer;
+import rx.Scheduler;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
 @Singleton
 public class DatabaseHelper {
     private final PreferencesHelper mPreferencesHelper;
-    private final EventPosterHelper mEventPoster;
+    private final RxEventBusHelper mEventPoster;
 
     @Inject
     public DatabaseHelper(PreferencesHelper preferencesHelper,
-                          EventPosterHelper eventPosterHelper) {
+                          RxEventBusHelper rxEventBusHelper) {
         mPreferencesHelper = preferencesHelper;
-        mEventPoster = eventPosterHelper;
+        mEventPoster = rxEventBusHelper;
     }
 
     public DBMovie findMovie(int remoteId) {
@@ -48,5 +57,23 @@ public class DatabaseHelper {
         DBMovie dbMovie = findMovie(movie.getId());
         if (dbMovie != null)
             dbMovie.delete();
+    }
+
+    public Observable<Movie> saveMovies(final List<Movie> movies) {
+        return Observable.create(new Observable.OnSubscribe<Movie>() {
+            @Override
+            public void call(Subscriber<? super Movie> subscriber) {
+                for (Movie movie : movies) {
+                    addMovie(movie);
+                    subscriber.onNext(movie);
+                }
+                postEvent(movies);
+                subscriber.onCompleted();
+            }
+        });
+    }
+
+    private void postEvent(List<Movie> movies) {
+        mEventPoster.post(movies);
     }
 }
