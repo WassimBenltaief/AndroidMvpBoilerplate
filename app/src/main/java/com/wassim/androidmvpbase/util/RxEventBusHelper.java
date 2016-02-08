a16707aa3cdf5c0ebc237e806d58dfa2d20c39f9
Wassim Beltaief <beltaief.wassin@gmail.com>
2016-02-08 18:06:36 +0100

    add PublishSubject with Scheduler.io() worker as poster on io() Scheduler (Temporary until future refactor for memory considerations )

--- app/src/main/java/com/wassim/androidmvpbase/util/RxEventBusHelper.java
+++ app/src/main/java/com/wassim/androidmvpbase/util/RxEventBusHelper.java
@@ -6,18 +6,22 @@ import android.os.Looper;
 import com.squareup.otto.Bus;
 
 import javax.inject.Inject;
+import javax.inject.Singleton;
 
 import rx.Observable;
+import rx.functions.Action0;
+import rx.schedulers.Schedulers;
 import rx.subjects.PublishSubject;
+import rx.subjects.SerializedSubject;
+import rx.subjects.Subject;
 
 /**
  * Provides helper methods to post event to an Otto event bus
  */
+@Singleton
 public class RxEventBusHelper {
 
-    private static RxEventBusHelper instance;
-
-    private PublishSubject<Object> subject = PublishSubject.create();
+    private final PublishSubject _bus = PublishSubject.create();
 
     @Inject
     public RxEventBusHelper() {
@@ -26,15 +30,25 @@ public class RxEventBusHelper {
     /**
      * Pass any event down to event listeners.
      */
-    public void post(Object object) {
-        subject.onNext(object);
+    public void send(final Object object) {
+        Schedulers.io().createWorker().schedule(new Action0() {
+            @Override
+            public void call() {
+                _bus. onNext(object);
+            }
+        });
+
     }
 
     /**
      * Subscribe to this Observable. On event, do something
      * e.g. replace a fragment
      */
-    public Observable<Object> get() {
-        return subject;
+    public Observable<Object> toObserverable() {
+        return _bus;
+    }
+
+    public boolean hasObservers(){
+        return _bus.hasObservers();
     }
 }
