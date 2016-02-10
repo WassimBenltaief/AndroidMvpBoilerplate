9e80f4dce9e8951502c70ce4b996174e855438cc
Wassim Beltaief <beltaief.wassin@gmail.com>
2016-02-10 12:04:17 +0100

    clean

--- app/src/androidTest/java/com/wassim/androidmvpbase/MainActivityTest.java
+++ app/src/androidTest/java/com/wassim/androidmvpbase/MainActivityTest.java
@@ -70,29 +70,4 @@ public class MainActivityTest {
                 .check(matches(isDisplayed()));
     }
 
-
-
-
-    /*@Test
-    public void listOfRibotsShows() {
-        List<Ribot> testDataRibots = TestDataFactory.makeListRibots(20);
-        when(component.getMockDataManager().getRibots())
-                .thenReturn(Observable.just(testDataRibots));
-
-        main.launchActivity(null);
-
-        int position = 0;
-        for (Ribot ribot : testDataRibots) {
-            onView(withId(R.id.recycler_view))
-                    .perform(RecyclerViewActions.scrollToPosition(position));
-            String name = String.format("%s %s", ribot.profile.name.first,
-                    ribot.profile.name.last);
-            onView(withText(name))
-                    .check(matches(isDisplayed()));
-            onView(withText(ribot.profile.email))
-                    .check(matches(isDisplayed()));
-            position++;
-        }
-    }*/
-
 }
